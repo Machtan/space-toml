e4d618848deafe1643f1dc4728c67c10ac7d4349
Machtan <jako3047@gmail.com>
2016-11-23 14:16:10 +0100

    Add example editing a cargofile

--- TODO.md
+++ TODO.md
@@ -1,3 +1,5 @@
 # Things that should be done
-- Implement 'Error' for the error types (instead of using 'show')
-- 
+- Implement `Error` for the error types (instead of using `show`)
+- Rename things prefixed with 'TOML'
+- Implement `Table.find_or_insert_array_of_tables`
+- Implement `Table.has_key`?
---
+++ examples/cargo.rs
@@ -0,0 +1,36 @@
+extern crate space_toml;
+
+use space_toml::{TomlTable};
+use std::env;
+use std::process;
+use std::io::{Read};
+use std::fs::File;
+
+fn main() {
+    let args = env::args().skip(1).collect::<Vec<_>>();
+    if args.len() < 1 {
+        println!("Usage: cargo run --example cargo -- [path/to/Cargo.toml]");
+        process::exit(1);
+    }
+    let ref path = args[0];
+    let mut file = File::open(path).expect("Could not open file");
+    let mut source = String::new();
+    file.read_to_string(&mut source).expect("Could not read file");
+    let mut table = space_toml::parse(&source).expect("Could not read TOML");
+    {
+        // Ensure that we don't borrow the table for too long
+        let mut dependencies = table.find_or_insert_table(&["dependencies"]).expect("Invalid file structure");
+        let mut dep = TomlTable::new_inline();
+        dep.insert("version", env!("CARGO_PKG_VERSION"));
+        dependencies.insert("space_toml", dep);
+        
+        let mut dep = TomlTable::new_inline();
+        dep.insert("path", "../rsdl2");
+        dependencies.insert("rsdl2", dep);
+    }
+
+    let mut output = String::new();
+    table.write(&mut output);
+    println!("~~~ Modified ~~~");
+    println!("{}", output);
+}
